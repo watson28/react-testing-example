dcb330bc5f908269a222aa33f96718fb01bd00a8
x220638 <jorge.florian2@wnco.com>
2017-08-15 17:17:37 -0500

    add testing with custom atmospheric pressure

--- test/water-test.js
+++ test/water-test.js
@@ -9,47 +9,77 @@ describe('Water component', function () {
     var mockProps = {
         initialTempeture: 18
     };
-    var mockPromise = new Promise(function (resolve, reject) {
-        resolve(760);
-    });
-    var mockweatherService = {
-        getAtmosphericPressure: function () {
-            return mockPromise;
-        }
-    };
 
-    beforeEach(function () {
-        Water.__set__({
-            envWeatherService: mockweatherService
+    describe('Default', function () {
+        beforeEach(function () {
+            var mockPromise = new Promise(function (resolve) {
+                resolve(760);
+            });
+            Water.__set__({
+                envWeatherService: {
+                    getAtmosphericPressure: function () {
+                        return mockPromise;
+                    }
+                }
+            });
+
+            renderedComponent = ReactTestUtils.renderIntoDocument(<Water {...mockProps} />);
+
+            return mockPromise;
         });
 
-        renderedComponent = ReactTestUtils.renderIntoDocument(<Water {...mockProps} />);
+        it('should render with initial state', function () {        
+            textElement = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'p');
+            expect(textElement.innerHTML).to.include('LIQUID');
+        });
 
-        return mockPromise;
+        it('should be gas when tempeture is above 100 celcious degree', function () {
+            var input = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'input');
+            input.value = '150';
+            
+            ReactTestUtils.Simulate.change(input);
+
+            textElement = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'p');
+            expect(textElement.innerHTML).to.include('GAS');
+        });
+
+        it('should be solid when tempeture is above 0 celcious degree', function () {
+            var input = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'input');
+            input.value = '-10';
+
+            ReactTestUtils.Simulate.change(input); 
+
+            textElement = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'p');
+            expect(textElement.innerHTML).to.include('SOLID');
+        });
     });
 
-    it('should render with initial state', function () {        
-        textElement = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'p');
-        expect(textElement.innerHTML).to.include('LIQUID');
-    });
+    describe('Custom atmospheric pressure', function () {
+        beforeEach(function () {
+            var mockPromise = new Promise(function (resolve) {
+                resolve(258.399); // atm pressure in everest
+            });
+            Water.__set__({
+                envWeatherService: {
+                    getAtmosphericPressure: function () {
+                        return mockPromise;
+                    }
+                }
+            });
 
-    it('should be gas when tempeture is above 100 celcious degree', function () {
-        var input = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'input');
-        input.value = '150';
-        
-        ReactTestUtils.Simulate.change(input);
+            renderedComponent = ReactTestUtils.renderIntoDocument(<Water {...mockProps} />);
 
-        textElement = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'p');
-        expect(textElement.innerHTML).to.include('GAS');
-    });
+            return mockPromise;
+        });
 
-    it('should be solid when tempeture is above 0 celcious degree', function () {
-        var input = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'input');
-        input.value = '-10';
+        it('should boil above 78°C in mount everest', function () {
+            var input = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'input');
+            input.value = '79';
+            
+            ReactTestUtils.Simulate.change(input);
 
-        ReactTestUtils.Simulate.change(input);
-
-        textElement = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'p');
-        expect(textElement.innerHTML).to.include('SOLID');
+            textElement = ReactTestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'p');
+            expect(textElement.innerHTML).to.include('GAS');
+        });
     });
 });
